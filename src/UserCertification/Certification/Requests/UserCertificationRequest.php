357972bdf3a1192176031352ccbc018b7d2cf7a9
汪晓雨 <15124532157@163.com>
2020-11-11 09:43:39 +0800

    调整实名验证

--- src/UserCertification/Certification/Requests/UserCertificationRequest.php
+++ src/UserCertification/Certification/Requests/UserCertificationRequest.php
@@ -13,21 +13,22 @@ class UserCertificationRequest extends FormRequest
             'name'       => 'required|min:2|max:5',
             'id_card'    => ['required', new IdCardRule()],
             'phone'      => [Rule::requiredIf(config('usercertification.open_phone_verified')), 'phone:CN,mobile'],
-            'front_card' => [Rule::requiredIf(config('usercertification.open_card_verified')), 'required'],
-            'back_card'  => [Rule::requiredIf(config('usercertification.open_card_verified')), 'required'],
+            'front_card' => [Rule::requiredIf(config('usercertification.open_card_verified'))],
+            'back_card'  => [Rule::requiredIf(config('usercertification.open_card_verified'))],
         ];
     }
 
     public function messages()
     {
         return [
-            'name.required'    => '认证用户姓名必须填写',
-            'name.min'         => '认证用户姓名至少:min个字符',
-            'name.max'         => '认证用户姓名最多:max个字符',
-            'id_card.required' => '身份证号必须填写',
-            'phone.phone'      => '手机号校验不通过',
+            'name.required'       => '认证用户姓名必须填写',
+            'name.min'            => '认证用户姓名至少:min个字符',
+            'name.max'            => '认证用户姓名最多:max个字符',
+            'id_card.required'    => '身份证号必须填写',
+            'phone.required'      => '手机号必须填写',
+            'phone.phone'         => '手机号校验不通过',
             'front_card.required' => '身份证正面图片必须上传',
-            'back_card.required' => '身份证背面图片必须上传',
+            'back_card.required'  => '身份证背面图片必须上传',
         ];
     }
 }
